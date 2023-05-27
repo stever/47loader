c313cf3fdf1064ef3470573cc2637a0f4920e4bb
Steven Robertson <stever@hey.com>
2023-05-27 18:09:50 +0100

    Restoring an earlier version of 'bidi-demo'.

--- examples/bidi-demo/build.sh
+++ examples/bidi-demo/build.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/zsh -fe
 
-# 47loader (c) Stephen Williams 2013-2015
+# 47loader (c) Stephen Williams 2013
 # See LICENSE for distribution terms
 
 prefix="bidi-demo"
-speed="STANDARD"
+speed="FAST"
 
 pasmo -E LOADER_SPEED_${speed}=1 $prefix.asm $prefix.bin
 
 # construct the BASIC loader with assembled binary embedded
 mono >$prefix.tzx ../../tools/47loader-bootstrap.exe \
-  <$prefix.bin -clear 32767 -pause 0 -name '47bidi' \
-  -border 0 -paper 0 -ink 7   
+  <$prefix.bin -clear 32767 -pause 0 -name '47bidi'
 
 # add the demo screen using bidirectional loading
 mono >>$prefix.tzx ../../tools/47loader-tzx.exe \
-  -speed $speed -fancyscreen bidi_pas4 \
+  -speed $speed -fancyscreen bidi_pac \
   =(cat ../../assets/penrose_pixmap.bin ../../assets/penrose_attrs.bin)
